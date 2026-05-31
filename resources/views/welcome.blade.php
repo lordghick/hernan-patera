fb1bed0fdba64442785f3c7dbf96212f89b4ab32
Lordghick <lordghick@gmail.com>
2026-05-31 14:26:07 +0200

    changed title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Natalia omg omg</title>
+    <title>Natalia parra</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
