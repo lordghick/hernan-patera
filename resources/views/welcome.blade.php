07ea1ce40d66935888c8528721a28b50ca054b05
Lordghick <lordghick@gmail.com>
2026-05-31 14:18:57 +0200

    confllict test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>La Patera</title>
+    <title>Poloilñi</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
